ee577b1e3e035455f32f69fcac8e7a6700825823
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-05-28 14:04:45 +0200

    hjsgfjhs

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,71 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 12:42:51 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/05/27 14:03:45 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/05/28 14:04:21 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#define BUFF_SIZE 4200
+#include "get_next_line.h"
+
+char	*get_line(char *dest, char *src)
+{
+	i = 0;
+	while (src[i] != '\n' && src[i] != '\0')
+		i++;
+	dest = (char*)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (src[i] != '\n' && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int		remove_until(char *str, int delimeter)
+{
+	int i;
+	int j;
+
+	j = 0;
+	i = 0;
+	while (str[i] != delimeter && str[i] != '\0')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	i++;
+	while (str[j])
+	{
+		if (str[i])
+			str[j] = str[i];
+		else
+			str[j] = '\0';
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (1);
+}
 
 int		get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	static char	arr[BUFF_SIZE + 1];
-	int			reat;
-	int			i;
+	static char	*text;
+	int			red;
 
-	i = 0;
-	reat = read(fd, buf, BUFF_SIZE);
-	buf[reat] = '\0';
-	if (buf[i] != '\0')
-		ft_strcpy(arr, buf);
-
+	(void)line;
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+		return (-1);
+	red = read(fd, buf, BUFF_SIZE);
+	buf[red] = '\0';
+	if (!text)
+	{
+		text = (char*)malloc(ft_strlen(buf + 1));
+		ft_strcpy(text, buf);
+	}
+	*line = get_line(*line, text);
+	if (remove_until(text, '\n') == 0)
+		return (0);
+	return (1);
 }
