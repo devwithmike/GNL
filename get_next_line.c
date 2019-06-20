650494489d60d4fc5958cda166fe3ac6221fd0fe
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-06-20 07:50:32 +0200

    fkhj

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,12 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 08:54:19 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/06/17 15:15:27 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/06/20 07:49:52 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
-char	*get_line(char *dest, char *src)
-{
-	int i;
-
-	i = 0;
-	while (src[i] != '\n' && src[i] != '\0' && src[i] != '\r')
-		i++;
-	dest = ft_strnew(i);
-	i = 0;
-	while (src[i] != '\n' && src[i] != '\0' && src[i] != '\r')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*remove_line(char *str)
-{
-	int i;
-	int j;
-
-	j = 0;
-	i = 0;
-	while (str[i] != '\n' && str[i] != '\0' && str[i] != '\r')
-		i++;
-	i++;
-	while (str[j])
-	{
-		if (str[i])
-			str[j] = str[i];
-		else
-			str[j] = '\0';
-		i++;
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
-}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -73,9 +32,4 @@ int		get_next_line(const int fd, char **line)
 		if (ft_strchr(text, '\n'))
 			break ;
 	}
-	if (ft_strlen(text) <= 0)
-		return (0);
-	*line = get_line(*line, text);
-	text = remove_line(text);
-	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 08:11:36 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/06/17 15:08:43 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/06/20 07:50:09 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include "libft/libft.h"
 # include <fcntl.h>
 
-char	*get_line(char *dest, char *src);
-char	*remove_line(char *str);
 int		get_next_line(const int fd, char **line);
 
 #endif
