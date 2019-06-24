ac9ac78edbbeaaf0ae5a93635a37058974fc08ab
Michael MEYER <mimeyer@c4r20s2.wethinkcode.co.za>
2019-06-24 10:13:55 +0200

    shgd

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,39 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 08:54:19 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/06/24 08:48:47 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/06/24 10:12:57 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+void	set_text(char *text[], char *temp, const int fd)
+{
+	ft_strdel(&text[fd]);
+	text[fd] = ft_strdup(temp);
+	ft_strdel(&temp);
+}
+
+int		read_file(char *text[], const int fd, char *temp)
+{
+	char	buf[BUFF_SIZE + 1];
+	int		red;
+
+	if ((red = read(fd, buf, BUFF_SIZE)) == 0)
+		return (0);
+	buf[red] = '\0';
+	temp = ft_strjoin(text[fd], buf);
+	set_text(text, temp, fd);
+	return (1);
+}
+
+int		single_line(char *text[], char **line, const int fd)
+{
+	*line = ft_strdup(text[fd]);
+	ft_strclr(text[fd]);
+	return (1);
+}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -19,36 +46,23 @@ int		get_next_line(const int fd, char **line)
 	static char	*text[1024];
 	char		*temp;
 	char		*ptr;
-	int			red;
 
+	temp = NULL;
 	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
 	if (!text[fd])
 		text[fd] = ft_strnew(0);
 	while ((ptr = ft_strchr(text[fd], '\n')) == NULL)
-	{
-		if ((red = read(fd, buf, BUFF_SIZE)) == 0)
+		if ((read_file(text, fd, temp)) == 0)
 			break ;
-		buf[red] = '\0';
-		temp = ft_strjoin(text[fd], buf);
-		ft_strdel(&text[fd]);
-		text[fd] = ft_strdup(temp);
-		ft_strdel(&temp);
-	}
 	if (ft_strlen(text[fd]) != 0)
 	{
 		if (!(ft_strchr(text[fd], '\n')))
-		{
-			*line = ft_strdup(text[fd]);
-			ft_strclr(text[fd]);
-			return (1);
-		}
+			return (single_line(text, &*line, fd));
 		*ptr = '\0';
 		temp = ft_strdup(ptr + 1);
 		*line = ft_strdup(text[fd]);
-		ft_strdel(&text[fd]);
-		text[fd] = ft_strdup(temp);
-		ft_strdel(&temp);
+		set_text(text, temp, fd);
 	}
 	else
 		return (0);
